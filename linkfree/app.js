c03abd7e466789c3847d91197fb9e982bef4afe4
dheeman-p <128053921+dheeman-p@users.noreply.github.com>
2023-12-16 19:12:05 +0530

    Add files via upload

---
+++ linkfree/app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+
+const AppError = require("./utils/appError");
+const globalErrorHandler = require("./controllers/errorController");
+
+const userRouter = require("./routes/userRoutes");
+
+const app = express();
+
+// Limit requests from same API
+
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: "10kb" }));
+
+app.use("/api/v1/users", userRouter);
+
+app.all("*", (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+app.use(globalErrorHandler);
+
+module.exports = app;
